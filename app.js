dd5c054ad8e5941697e0bdec08fad1c80db6660b
kamiokz123 <kamranhaiderokz888@gmail.com>
2024-11-17 23:29:55 +0500

    some new changes

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const engine = require('ejs-mate');
 const Listing = require("./models/listing.js");
 const path = require("path");
+let methodOverride = require('method-override')
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+// Set the 'public' folder as the directory for serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+// use ejs-locals for all ejs templates:
+app.engine('ejs', engine);
+
+
 
 
 main().then(()=>{
@@ -28,6 +42,12 @@ app.get("/listings",async (req,res)=>{
     res.render("./listing/index.ejs",{allList});
 });
 
+// new route
+app.get("/listings/new",(req,res)=>{
+    res.render("./listing/new.ejs");
+});
+
+
 // show indivisual route
 app.get("/listings/:id",async (req,res)=>{
     let { id } = req.params;
@@ -35,6 +55,34 @@ app.get("/listings/:id",async (req,res)=>{
     console.log(list);
     res.render("./listing/show.ejs",{list});
 });
+
+// adding new list
+app.post("/listings",async (req,res)=>{
+    console.log(req.body.listing);
+    
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+// edit route
+app.get("/listings/:id/edit",async(req,res)=>{
+    const {id} = req.params;
+    const list = await Listing.findById(id);
+    console.log(list);
+    res.render("./listing/edit.ejs",{list})
+});
+
+// update route
+app.put("/listings/:id",async (req,res)=>{
+    const {id} = req.params;
+    const updatedList = req.body.listing;
+    console.log({...updatedList,image:{filename:"",url:""}});
+    const list = await Listing.findByIdAndUpdate(id,{...updatedList,image:{filename:"",url:""}});
+    res.redirect(`/listings/${id}`)
+});
+
+
 
 // app.get("/newListing",async (req,res)=>{
 //     const testList = new Listing({
