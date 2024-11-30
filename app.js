7fdceeb51eb1b2c0dfe35e42db677da377ba564b
kamiokz123 <kamranhaiderokz888@gmail.com>
2024-11-30 09:36:57 +0500

    updated the error handlers

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const mongoose = require("mongoose");
 const engine = require('ejs-mate');
 const Listing = require("./models/listing.js");
 const path = require("path");
-let methodOverride = require('method-override')
+let methodOverride = require('method-override');
+const asyncWraper = require("./utils/asyncWrap.js");
+const ExpressError = require("./utils/ExpressError.js");
+const listingSchema = require("./schema.js");
+
+
 
 
 app.set('view engine', 'ejs');
@@ -22,87 +27,91 @@ app.engine('ejs', engine);
 
 
 
-main().then(()=>{
+main().then(() => {
     console.log("connected to db");
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err);
 });
 
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
-  }
+}
 
-app.get("/",(req,res)=>{
+const validateListing = (req,res,next)=>{
+    const result = listingSchema.validate(req.body);
+    console.log(result);
+    if (result.error) {
+        throw new ExpressError(400, result.error);
+    }else{
+        next();
+    }
+};
+
+app.get("/", (req, res) => {
     res.send("root page");
 });
 
 // show all listing route
-app.get("/listings",async (req,res)=>{
-    const allList = await Listing.find();    
-    res.render("./listing/index.ejs",{allList});
-});
+app.get("/listings", asyncWraper(async (req, res) => {
+    const allList = await Listing.find();
+    // console.log(allList);
+    res.render("./listing/index.ejs", { allList });
+}));
 
 // new route
-app.get("/listings/new",(req,res)=>{
+app.get("/listings/new", (req, res) => {
     res.render("./listing/new.ejs");
 });
 
 
 // show indivisual route
-app.get("/listings/:id",async (req,res)=>{
+app.get("/listings/:id", asyncWraper(async (req, res) => {
     let { id } = req.params;
     const list = await Listing.findById(id);
     console.log(list);
-    res.render("./listing/show.ejs",{list});
-});
+    res.render("./listing/show.ejs", { list });
+}));
 
 // adding new list
-app.post("/listings",async (req,res)=>{
-    console.log(req.body.listing);
-    
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-});
+app.post("/listings", validateListing , asyncWraper(async (req, res, next) => {
+        const newListing = new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/listings");
+}));
 
 // edit route
-app.get("/listings/:id/edit",async(req,res)=>{
-    const {id} = req.params;
+app.get("/listings/:id/edit", asyncWraper(async (req, res) => {
+    const { id } = req.params;
     const list = await Listing.findById(id);
     console.log(list);
-    res.render("./listing/edit.ejs",{list})
-});
+    res.render("./listing/edit.ejs", { list })
+}));
 
 // update route
-app.put("/listings/:id",async (req,res)=>{
-    const {id} = req.params;
+app.put("/listings/:id", validateListing , asyncWraper(async (req, res) => {
+    const { id } = req.params;
     const updatedList = req.body.listing;
-    console.log({...updatedList,image:{filename:"",url:""}});
-    const list = await Listing.findByIdAndUpdate(id,{...updatedList,image:{filename:"",url:""}});
-    res.redirect(`/listings/${id}`)
-});
+    const list = await Listing.findByIdAndUpdate(id, { ...updatedList, image: { filename: "", url: "" } });
+    res.redirect(`/listings/${id}`);
+}));
 
-app.delete("/listings/:id",async (req,res)=>{
-    const {id} = req.params;
+app.delete("/listings/:id", asyncWraper(async (req, res) => {
+    const { id } = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect(`/listings`)
+}));
+
+
+// error handling middlewares
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "page not found"));
 });
 
+app.use((err, req, res, next) => {
+    const { statusCode = 500, message = "some error occured" } = err;
+    res.status(statusCode).render("./error.ejs", { message });
+});
 
-// app.get("/newListing",async (req,res)=>{
-//     const testList = new Listing({
-//         title : "my dubai villa",
-//         description : "by the beach",
-//         price:2600,
-//         location:"karachi , afghanistan",
-//         country: "afghanistan"
-//     });
-//     await testList.save();
-//     console.log("sample saved");
-    
-//     res.send("added new list")
-// });
-
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log("listening on 8080");
 });
